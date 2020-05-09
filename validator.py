70c72459173a4d3f0486596bec9a89583e4f2c63
demingry <demingry@sina.com>
2020-05-09 12:17:43 +0800

    Update validator.py

--- validator.py
+++ validator.py
@@ -7,13 +7,13 @@ import smtplib
 from email.mime.text import MIMEText
 from email.header import Header
 
-url='http://222.206.176.104/jsxsd/xspj/xspj_list.do?pj0502id=5001432D2FF745C0AAA60C041FA682DF&pj01id=61EC521F3E214FBCAF8CF3C66088A540&xnxq01id=2019-2020-1'
+url=''
 header={'Accept':'text/html, application/xhtml+xml, application/xml; q=0.9, */*; q=0.8','Accept-Encoding':'gzip, deflate','Accept-Language': 'zh-CN','Cache-Control': 'max-age=0','Connection': 'Keep-Alive','Cookie': 'JSESSIONID=A2D0D1F80BE6BCA4B61D5D8C82B8B241','Host': '222.206.176.104','Upgrade-Insecure-Requests': '1','User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/64.0.3282.140 Safari/537.36 Edge/18.17763'}
-mail_host = "smtp.163.com"
-mail_user = "hezeuser@163.com"
-mail_pass = "2016zb11"
-sender = 'hezeuser@163.com'
-receivers = ['1353217661@qq.com']
+mail_host = "[SMTPServer]"
+mail_user = "[YourEmail]"
+mail_pass = "[Your Password]"
+sender = '[MailSender]'
+receivers = ['PostMaster@qq.com']
 content = 'accessible'
 title = 'notice!'
 
